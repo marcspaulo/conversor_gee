246bf5ad71e3dff5939567e31168f9a6227af878
Marcos Paulo <marcospaulo.silvavieira@yahoo.com.br>
2025-11-03 23:24:23 -0300

    Ajustando algumas variáveis e títulos

--- App.js
+++ App.js
@@ -4,27 +4,43 @@ import { StyleSheet, Text, View } from 'react-native';
 import { TextInput } from 'react-native-web';
 
 export default function App() {
-  const[valoreletricidade,setvaloreletricidade] = useState('');
+  const [valoreletricidade, setvaloreletricidade] = useState('');
 
   return (
-    <View style={styles.container}>
-      <Text>Ola mundo</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text >Consumo de energia</Text>
+      <TextInput
+        style={styles.input}
+        placeholderTextColor={'#fc0707ff'}
+        placeholder="Insira o consumo em Kwh"
+        keyboardType='numeric'
+        value={valoreletricidade}
+        onChangeText={setvaloreletricidade}
+      ></TextInput>
 
-    <Text >Eletricidade</Text>
-    <TextInput 
-    style={styles.input}
-    placeholder="Insira o consumo em Kwh"
-    keyboardType='numeric'
-    value={valoreletricidade}
-    onChangeText={setvaloreletricidade}
-    >
-    </TextInput>
+      <Text>Consumo de água</Text>
+      <TextInput
+        placeholder="Insira o consumo em m3"
+      ></TextInput>
 
-    <text>você digitou: {valoreletricidade}</text>
-    
+
+      <Text>Consumo de combustível</Text>
+      <TextInput
+        placeholder="Insira o valor de combustível gasto"
+      ></TextInput>
+
+
+      <Text>Gás de cozinha</Text>
+      <TextInput
+        placeholder="Insira o valor"
+      ></TextInput>
+
+
+      <text>você digitou: {valoreletricidade}</text>
     </View>
+
   );
+
 }
 
 const styles = StyleSheet.create({
